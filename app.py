808573b8ebbf2394757b5779687bfcf3fccd73ba
Surendar-G-4899 <85760934+Surendar-G-4899@users.noreply.github.com>
2021-06-12 12:55:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ student_attendance={}
 
 #-----------------------Initial decleration and preperation----------------------
 
-face1_image = face_recognition.load_image_file(".<Facimage1_path>.jpg")
-face1_face_encoding = face_recognition.face_encodings(face1_image)[0]
+surendar_image = face_recognition.load_image_file("./sp/images/surendar.jpg")
+surendar_face_encoding = face_recognition.face_encodings(surendar_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-face2_image = face_recognition.load_image_file("./sp/images/vasanth.jpg")
-face2_face_encoding = face_recognition.face_encodings(face2_image)[0]
+vasanth_image = face_recognition.load_image_file("./sp/images/vasanth.jpg")
+vasanth_face_encoding = face_recognition.face_encodings(vasanth_image)[0]
 
 # Load a third sample picture and learn how to recognize it.
-face3_image = face_recognition.load_image_file("./sp/images/vishnu.jpg")
-face3_face_encoding = face_recognition.face_encodings(face3_image)[0]
+vishnu_image = face_recognition.load_image_file("./sp/images/vishnu.jpg")
+vishnu_face_encoding = face_recognition.face_encodings(vishnu_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
 #mnivetha_image = face_recognition.load_image_file("./np/images/m_nivetha1.jpg")
@@ -41,24 +41,23 @@ face3_face_encoding = face_recognition.face_encodings(face3_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    face1_face_encoding,
-    face2_face_encoding,
-    face3_face_encoding
-    
+    surendar_face_encoding,
+    vasanth_face_encoding,
+    vishnu_face_encoding
+    #mnivetha_face_encoding
 ]
-## Enter Name of faces in order
 known_face_names = [
-    "face1name",
-    "face2name",
-    "face3name"
-    
+    "Surendar",
+    "Vasanth",
+    "Vishnu"
+    #"M_Nivetha"
 ]
 
 
 #---------------------------Define Global Attendance Dictionary--------------------
 def Initial_set():
     for names in known_face_names:
-        student_attendance[names]=[[0,0,0],"absent"]
+        student_attendance[names]=[[1,1,1],"present"] #change for attendance
 
     # Set Unknown intimation for error correction (Faces)
     student_attendance["Unknown"]=[[0,0,0],"absent"]
@@ -75,77 +74,72 @@ def capture_session_screen():
     face_encodings = []
     face_names = []
     process_this_frame = True
-    name_set=[]
 
-    try:
+    video_capture = cv2.VideoCapture(1)
+    ret, frame = video_capture.read()
 
-        video_capture = cv2.VideoCapture(1)
-        ret, frame = video_capture.read()
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+            face_names.append(name)
+    process_this_frame = not process_this_frame
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
-                face_names.append(name)
-        process_this_frame = not process_this_frame
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    # Display the resulting image
+    frame=cv2.resize(frame,(960,540))
+    #cv2.imshow('Video', frame)
 
-        # Display the resulting image
-        frame=cv2.resize(frame,(960,540))
-        #cv2.imshow('Video', frame)
+    # Hit 'q' on the keyboard to quit!
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
 
-        # Hit 'q' on the keyboard to quit!
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
+    name_set=set(face_names)
+    print(name_set)
+    #break
 
-        name_set=set(face_names)
-        print(name_set)
-        #break
-
-        # Release handle to the webcam
-        video_capture.release()
-        #cv2.destroyAllWindows()
-        #cv2.waitKey(0)
-    except:
-        print("error occured with your camera")
+    # Release handle to the webcam
+    video_capture.release()
+    #cv2.destroyAllWindows()
+    #cv2.waitKey(0)
 
     return name_set
 
